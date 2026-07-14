70cda671dd862ff6b08f9352f15d59cab242320c
rashiktalukder <kawsar.rashik@gmail.com>
2026-07-15 01:59:08 +0600

    Add project files.

---
+++ RoyalVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoyalVilla_API.Controllers
+{
+    [Route("api/villa")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public string GetVillas()
+        {
+            return "Get All Villas";
+        }
+    }
+}
